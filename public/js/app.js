c583fc856995cba00c4a9e09a423706a4cced2d0
Chris Kim <chrisdjkim@gmail.com>
2020-10-07 18:07:28 -0400

    Removed unneccessary console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("JavaScript file is loaded");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const weatherMessage = document.querySelector("#weather-message");
